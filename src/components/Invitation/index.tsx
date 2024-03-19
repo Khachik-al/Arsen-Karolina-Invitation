04c8d4a502d4bf1344ac96966c2fc870082652e1
Tigran Parsadanyan <tigran.parsadanyan@inconceptlabs.com>
2024-03-19 12:45:05 +0400

    Add text size to each invitation title

--- src/components/Invitation/index.tsx
+++ src/components/Invitation/index.tsx
@@ -18,10 +18,10 @@ export default function Invitation({ locale }: LocaleProps) {
       <div className="h-full w-full bg-gradient-to-t from-[#000000]/40 to-[#000000]/0 flex flex-col justify-end items-center text-white gap-[35px] py-[30px]">
         <div className="flex flex-col items-center gap-[18px]">
           <span className={clsx(
-          'lg:text-8xl px-5 lg:p-0 text-center [text-shadow:_0_8px_8px_rgb(0_0_0_/_80%)]',
-          locale === 'hy' && 'text-5xl font-tumanyan',
-          locale === 'ru' && 'text-4xl font-pompadur',
-          locale === 'en' && 'text-6xl font-beauty_hand_writing',
+          'px-5 lg:p-0 text-center [text-shadow:_0_8px_8px_rgb(0_0_0_/_80%)]',
+          locale === 'hy' && 'text-5xl lg:text-8xl font-tumanyan',
+          locale === 'ru' && 'text-3xl lg:text-6xl font-pompadur',
+          locale === 'en' && 'text-6xl lg:text-8xl font-beauty_hand_writing',
           )}>
             {t('title')}
           </span>
