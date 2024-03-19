96700d088992d1123fb20a570cf9fe5d4f9f4620
Tigran Parsadanyan <tigran.parsadanyan@inconceptlabs.com>
2024-03-19 11:24:26 +0400

    Change wedding_time_left property to title and update text styles

--- src/components/Invitation/index.tsx
+++ src/components/Invitation/index.tsx
@@ -1,5 +1,7 @@
 import { useTranslations } from 'next-intl';
 
+import clsx from 'clsx';
+
 import type { LocaleProps } from '@/app/[locale]/layout';
 
 import TimeCounter from '@/components/Invitation/elements/TimeCounter';
@@ -15,8 +17,13 @@ export default function Invitation({ locale }: LocaleProps) {
       <Audio />
       <div className="h-full w-full bg-gradient-to-t from-[#000000]/40 to-[#000000]/0 flex flex-col justify-end items-center text-white gap-[35px] py-[30px]">
         <div className="flex flex-col items-center gap-[18px]">
-          <span className="text-2xl lg:text-3xl px-5 lg:p-0 font-light text-center">
-            {t('wedding_time_left')}
+          <span className={clsx(
+          'lg:text-8xl px-5 lg:p-0 text-center [text-shadow:_0_8px_8px_rgb(0_0_0_/_80%)]',
+          locale === 'hy' && 'text-5xl font-tumanyan',
+          locale === 'ru' && 'text-4xl font-pompadur',
+          locale === 'en' && 'text-6xl font-beauty_hand_writing',
+          )}>
+            {t('title')}
           </span>
         </div>
         <TimeCounter />
